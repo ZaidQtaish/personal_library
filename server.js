233565778cbce4a4adb9c7bf987766ff89110741
xFxdedFade <enderzaide@gmail.com>
2024-12-18 05:45:43 +0300

    fix: removed fcc tests

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to Database'))
   .catch(err => console.error('Database Connection Error', err));
 
-// For FCC testing purposes
-fccTestingRoutes(app);
-
 // Routing for API 
 apiRoutes(app);  
     
